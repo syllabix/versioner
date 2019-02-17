8a80bc45427fefd63fd44caef16c60c20eb74ed3
Tom Stoepker <tom.stoepker@gmail.com>
2019-02-17 19:43:42 +0100

    fix: print abbreviated annoted as suffix to avoid version parse error

--- tag/tag.go
+++ tag/tag.go
@@ -10,7 +10,7 @@ import (
 // latest annotated tag, returning an empty version and
 // non nil error in the event of failure
 func GetLatest() (string, error) {
-	cmd := exec.Command("git", "describe")
+	cmd := exec.Command("git", "describe", "--abbrev=0")
 	tag, err := cmd.Output()
 	if err != nil {
 		return "", err
