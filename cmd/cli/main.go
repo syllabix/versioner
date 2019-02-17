a7a264ba121a5586cfacf8003e1d6185126f3518
Tom Stoepker <tom.stoepker@gmail.com>
2019-02-17 22:52:27 +0100

    feat: changelog generator

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,16 +4,24 @@ import (
 	"flag"
 	"os"
 
+	"github.com/syllabix/versioner/changelog"
+	"github.com/syllabix/versioner/commit"
+	"github.com/syllabix/versioner/semver"
+	"github.com/syllabix/versioner/tag"
+
 	"github.com/syllabix/versioner/internal/diagnostic"
 
 	"github.com/fatih/color"
-	"github.com/syllabix/versioner"
 )
 
 func main() {
 
 	var showVersion bool
 	flag.BoolVar(&showVersion, "v", false, "print the current version")
+
+	var logtitle string
+	flag.StringVar(&logtitle, "-o", "CHANGELOG.md", "sets the name of the output file")
+
 	flag.Parse()
 
 	if flag.Arg(0) == "version" {
@@ -25,10 +33,47 @@ func main() {
 		os.Exit(0)
 	}
 
-	version, err := versioner.Next()
+	var to string
+	vTag, err := tag.GetLatest()
+	if err == nil {
+		to = vTag
+	} else {
+		// no tags were found - assuming new project
+		vTag = "0.0.0"
+	}
+
+	version, err := semver.Parse(vTag)
 	if err != nil {
-		color.Red("unable to determine next version\n%v", err)
+		color.Red("%+v", err)
 		os.Exit(1)
 	}
-	color.Green("%s", version)
+
+	msgs, err := commit.MessagesInRange("HEAD", to)
+	if err != nil {
+		color.Red("%+v", err)
+		os.Exit(1)
+	}
+
+	vnext, err := semver.ComputeNext(version, msgs)
+	if err != nil {
+		color.Red("%+v", err)
+		os.Exit(1)
+	}
+
+	generator := changelog.NewGenerator(vnext, msgs)
+
+	f, err := os.Create(logtitle)
+	if err != nil {
+		color.Red("%+v", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	err = generator.Generate(f)
+	if err != nil {
+		color.Red("%+v", err)
+		os.Exit(1)
+	}
+
+	color.Green("%s", vnext)
 }
